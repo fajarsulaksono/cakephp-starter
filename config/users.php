e51057b5c380152c0a98bce5a4a9ef41948e4914
Fajar Sulaksono <fajar.sulaksono@gmail.com>
2019-02-14 00:56:33 +0700

    add config for cakeDC/users

---
+++ config/users.php
@@ -0,0 +1,8 @@
+<?php
+return [
+  'OAuth.providers.facebook.options.clientId' => 'YOUR APP ID',
+  'OAuth.providers.facebook.options.clientSecret' => 'YOUR APP SECRET',
+  'OAuth.providers.twitter.options.clientId' => 'YOUR APP ID',
+  'OAuth.providers.twitter.options.clientSecret' => 'YOUR APP SECRET',
+  //etc
+];
